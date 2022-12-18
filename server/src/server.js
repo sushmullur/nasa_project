44552e7894f845478a1982467e071aac67a81dd7
sushpicious <2011658@apps.nsd.org>
2022-12-18 01:01:33 -0800

    GET /planets

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const app = require('./app');
 
+// Port will be either 8000 or environment specified in the config.
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
@@ -10,6 +11,8 @@ const server = http.createServer(app);
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
+
+
 
 
 
