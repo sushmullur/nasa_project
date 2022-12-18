7a8beeebf76b903442f442685e8469f48f2350a1
sushpicious <2011658@apps.nsd.org>
2022-12-18 12:17:27 -0800

    Serving Front End in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const cors = require('cors');
 
 
@@ -8,8 +10,13 @@ const planetsRouter = require('./routes/planets/planets.router');
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
-app.use(express.json());
-app.use(planetsRouter);
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
